b0cb50a457629cc18074a92c609459e04dce7059
Robert Parsons <rmpcoding@gmail.com>
2021-03-23 23:57:17 -0700

    Adds working addToDo() function in App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+    const [input, setInput] = useState('');
+
+    const addToDo = (e) => {
+        e.preventDefault();
+        console.log(`This is the input: ${input}`);
+        console.log(todos);
+        setTodos([...todos, input]);
+        setInput("");
+        console.log(todos);
+    };
+
+    return (
+        <div className="App">
+            <h1>Welcome to my to-do list</h1>
+            <form>
+                <input
+                    type="text"
+                    className="input"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                ></input>
+                <button type="submit" className="button" onClick={addToDo}>
+                    Add to do
+                </button>
+            </form>
+            <h2>List of todos:</h2>
+            {todos.map((todo) => (
+                <p>{todo}</p>
+            ))}
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +10,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+
+The ability to type into an input field and hit the ENTER key to add a TODO to a list of TODO's
+The ability to delete a todo from the todo list
+The ability to update a todo from the todo list
+Each TODO must automatically be capitalised when added to the list of TODO's
+There should be a button on each TODO to indicate completion of a TODO, the text should turn green and have a strikethrough once completed
+The TODO app should be styled appropriately (you could use Glassmorphism!)
+At the top of the page, the client wants an inspirational quote which is different each time the user refreshes the page. The quote should be fetched from the following API: https://type.fit/api/quotes
+CHALLENGE: Persist the TODO's so that when the user refreshes the page, the list of TODO's is preserved!
+CHALLENGE 2: Add animations to the TODO list using this library!
+
+*/
